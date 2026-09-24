ef9d3d1b167ca511aea0606ac852acd5769c9893
DiegoPowerDev <diego_torres_11@hotmail.com>
2026-09-24 02:13:30 -0500

    mejorando manejo de url del backend

--- src/content/api.ts
+++ src/content/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    process.env.API_URL ||
+    "http://localhost:3010",
   withCredentials: true,
   timeout: 10000,
 });
