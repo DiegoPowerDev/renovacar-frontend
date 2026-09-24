92ccaa3f0281452f54f793eac082e0a64b4f3382
DiegoPowerDev <diego_torres_11@hotmail.com>
2026-09-24 02:36:01 -0500

    solucionando url

--- src/components/footers/home-footer.tsx
+++ src/components/footers/home-footer.tsx
@@ -2,9 +2,7 @@ export default function HomeFooter() {
   return (
     <div className="h-12 max-w-7xl w-full items-center  flex p-4">
       <div className="flex w-full justify-end">
-        {process.env.NODE_ENV}
         <div>Website developed by Diego Torres</div>
-        {process.env.API_URL}
       </div>
     </div>
   );
